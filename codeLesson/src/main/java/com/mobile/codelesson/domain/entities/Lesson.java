78e5a7174051d95ed3367f4e4fe88fb14be93837
Nietoof6 <00087419@uca.edu.sv>
2024-06-18 01:04:03 -0600

    fix: fixing lesson entity

--- codeLesson/src/main/java/com/mobile/codelesson/domain/entities/Lesson.java
+++ codeLesson/src/main/java/com/mobile/codelesson/domain/entities/Lesson.java
@@ -1,11 +1,13 @@
 package com.mobile.codelesson.domain.entities;
 
+import jakarta.persistence.Id;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "lessons")
 public class Lesson {
+    @Id
     private String id;
     private String title;
     private String lesson;
