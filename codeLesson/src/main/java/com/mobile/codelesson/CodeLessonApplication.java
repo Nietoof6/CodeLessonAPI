0a2a939ca590b2ef23205d9e86cc7361f6974ac2
Nietoof6 <00087419@uca.edu.sv>
2024-06-18 00:06:03 -0600

    create: create connection with mongo atlas & base api

---
+++ codeLesson/src/main/java/com/mobile/codelesson/CodeLessonApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.codelesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLessonApplication.class, args);
+	}
+
+}
---
+++ codeLesson/src/test/java/com/mobile/codelesson/CodeLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.codelesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
